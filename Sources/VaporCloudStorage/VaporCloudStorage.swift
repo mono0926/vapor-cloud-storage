08bc9a868e42ec69966ab78da465a7fa1df3527c
Masayuki Ono <mono0926@gmail.com>
2017-10-17 09:20:12 +0900

    Add get/post method

--- Sources/VaporCloudStorage/VaporCloudStorage.swift
+++ Sources/VaporCloudStorage/VaporCloudStorage.swift
@@ -1,3 +1,71 @@
-struct VaporCloudStorage {
-    var text = "Hello, World!"
+import Vapor
+import Foundation
+import Console
+import HTTP
+import Debugging
+import Multipart
+
+public protocol CloudStrageClinet {
+    func get(authToken: String,
+             object: String) throws -> Response
+    func post(authToken: String,
+              object: String,
+              data: Bytes,
+              predefinedAcl: String?,
+              cacheControl: String?) throws -> Response
+}
+
+public struct CloudStrageRestClient: CloudStrageClinet {
+    private let baseURL: URL
+    private let uploadURL: URL
+    private let client: ClientFactoryProtocol
+    private let logger: LogProtocol
+
+    public init(bucket: String,
+                client: ClientFactoryProtocol = EngineClientFactory(),
+                logger: LogProtocol = ConsoleLogger(Terminal(arguments: []))) {
+        self.baseURL = URL(string: "https://www.googleapis.com/storage/v1/b/\(bucket)/o")!
+        self.uploadURL = URL(string: "https://www.googleapis.com/upload/storage/v1/b/\(bucket)/o")!
+        self.client = client
+        self.logger = logger
+    }
+
+    public func get(authToken: String,
+                    object: String) throws -> Response {
+        let res = try client.get("\(baseURL.absoluteString)/\(object.addingPercentEncoding(withAllowedCharacters: .urlHostAllowed)!)")
+        logger.debug(res.body.bytes?.makeString() ?? "")
+        return res
+    }
+
+    public func post(authToken: String,
+                     object: String,
+                     data: Bytes,
+                     predefinedAcl: String? = nil,
+                     cacheControl: String? = nil) throws -> Response {
+        let request = Request(method: .post,
+                              uri: uploadURL.absoluteString,
+                              headers: createHeaders(authToken: authToken))
+        var query: Node = [
+            "uploadType": "multipart",
+            "name": Node(object)]
+        if let predefinedAcl = predefinedAcl {
+            query["predefinedAcl"] = Node(predefinedAcl)
+        }
+        request.query = query
+        var json = JSON()
+        if let cacheControl = cacheControl {
+            try json.set("cacheControl", cacheControl)
+        }
+        request.multipart = [Part(headers: ["Content-Type": "application/json"],
+                                  body: try json.makeBytes()),
+                             Part(headers: [:], body: data)
+        ]
+        let res = try client.respond(to: request)
+        logger.debug(res.body.bytes?.makeString() ?? "")
+        return res
+    }
+
+    private func createHeaders(authToken: String) -> [HeaderKey: String] {
+        return ["Authorization": "Bearer \(authToken)"]
+    }
 }
--- Tests/VaporCloudStorageTests/VaporCloudStorageTests.swift
+++ Tests/VaporCloudStorageTests/VaporCloudStorageTests.swift
@@ -1,16 +1,28 @@
 import XCTest
-@testable import VaporCloudStorage
+import VaporCloudStorage
+
+private let bucket = "YOUR_BUCKET"
+private let authToken = "YOUR_AUTH_TOKEN"
 
 class VaporCloudStorageTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(VaporCloudStorage().text, "Hello, World!")
+    let target: CloudStrageClinet = CloudStrageRestClient(bucket: bucket)
+
+    func test() throws {
+
+        let responsePost = try target.post(authToken: authToken,
+                                           object: "test/test.png",
+                                           data: FileManager.default.contents(atPath: "/Users/mono/Desktop/love.png")!.makeBytes(),
+                                           predefinedAcl: "publicRead",
+                                           cacheControl: "public, max-age=31536000") // one year
+        XCTAssertEqual(responsePost.status.statusCode, 200)
+
+        let responseGet = try target.get(authToken: authToken,
+                                         object: "test/test.png")
+        XCTAssertEqual(responseGet.status.statusCode, 200)
     }
 
 
     static var allTests = [
-        ("testExample", testExample),
+        ("test", test),
     ]
 }
